e75d65894a7115be6ae40e1be96b0c09d4ea45d5
Pablo Poggio <pablopoggiog@gmail.com>
2021-09-30 19:20:27 -0300

    Create Button

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 export const Button = styled.button`
   border-radius: 5px;
-  background-color: #0032eb;
+  background-color: #63b5cf;
   padding: 10px;
   color: white;
   cursor: pointer;
   transition: 0.6s;
 
   &:hover {
-    background-color: #335bef;
+    background-color: #3ca2c3;
     transform: scale(1.01);
   }
 `;
