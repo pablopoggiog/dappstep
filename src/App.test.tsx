aa9f90621397f9beb3600df025445b6c9c13609c
Pablo Poggio <pablopoggiog@gmail.com>
2021-09-30 21:30:22 -0300

    Fix sample test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders page's title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/Let's wave/i);
+  expect(title).toBeInTheDocument();
 });
